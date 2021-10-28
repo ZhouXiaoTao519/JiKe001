07696e305e877b8dfe2679b5b36c9c7629f2097f
赵攀 <853905045@qq.com>
2021-10-28 20:56:56 +0800

    '新闻详情'

--- js/车型概览.js
+++ js/车型概览.js
@@ -18,16 +18,21 @@ $(function () {
   touch.on($(".gb"), "tap", function (ev) {
     $("nav").hide();
   });
-    touch.on($(".ip6"), "tap", function (ev) {
+  touch.on($(".ip6"), "tap", function (ev) {
     var isChecked = $('.checkbox').prop('checked');
-    if(isChecked==true){
-alert('提交成功！')
-    }else{
+    if (isChecked == true) {
+      alert('提交成功！')
+    } else {
       alert('请仔细阅读《隐私条款》')
     }
   })
 
 });
+// $(function () {
+//   $('.btn').on('click', function () {
+//     window.location.href = "./新闻详情.html";
+//   })
+// })
 
 
 
