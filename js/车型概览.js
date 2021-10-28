df4cc32cf6265514dc6bc7e414d323752002a9ef
‘ZhouTao <1076524679@qq.com>
2021-10-28 16:45:13 +0800

    '完了'

--- js/车型概览.js
+++ js/车型概览.js
@@ -14,4 +14,16 @@ $(function () {
   touch.on($(".gb"), "tap", function (ev) {
     $("nav").hide();
   });
+    touch.on($(".ip6"), "tap", function (ev) {
+    
+    var isChecked = $('.checkbox').prop('checked');
+    if(isChecked==true){
+alert('提交成功！')
+    }else{
+      alert('请仔细阅读《隐私条款》')
+    }
+  })
 });
+
+
+
