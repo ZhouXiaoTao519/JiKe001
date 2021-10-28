43c2d0a2febab8958474eae783b89b305cc9cd9c
周彤 <707978257@qq.com>
2021-10-28 16:37:09 +0800

    'zt1'

--- js/车型概览.js
+++ js/车型概览.js
@@ -1,11 +1,15 @@
 $(function () {
   touch.on($(".player"), "tap", function (ev) {
+    var music = document.getElementById("music");
     if ($(".player").is(".player-start")) {
       $(".player").addClass("player-stop").removeClass("player-start");
+      music.pause();
     } else if ($(".player").is(".player-stop")) {
       $(".player").addClass("player-start").removeClass("player-stop");
+      music.play();
     } else {
       $(".player").addClass("player-start");
+      music.play();
     }
   });
   touch.on($(".cd"), "tap", function (ev) {
@@ -15,3 +19,12 @@ $(function () {
     $("nav").hide();
   });
 });
+function setRem() {
+  // 设计稿宽度和设备宽度
+  var uiW = 375,
+    winW = document.documentElement.clientWidth,
+    // 定义比例
+    rate = winW / uiW;
+  document.documentElement.style.fontSize = rate * 10 + "px";
+}
+setRem();
