5c7edd982f05b80d437439f1e2ff8e18ac1beef7
‘ZhouTao <1076524679@qq.com>
2021-10-29 11:45:23 +0800

    'asd'

--- js/车型概览.js
+++ js/车型概览.js
@@ -18,18 +18,39 @@ $(function () {
   touch.on($(".gb"), "tap", function (ev) {
     $("nav").hide();
   });
-    touch.on($(".ip6"), "tap", function (ev) {
-    var isChecked = $('.checkbox').prop('checked');
-    if(isChecked==true){
-alert('提交成功！')
-    }else{
-      alert('请仔细阅读《隐私条款》')
+  touch.on($(".ip6"), "tap", function (ev) {
+    var isChecked = $(".checkbox").prop("checked");
+    if (isChecked == true) {
+      alert("提交成功！");
+    } else {
+      alert("请仔细阅读《隐私条款》");
     }
-  })
-
+  });
+  // $()
+  $("body").bind("touchmove", function (e) {
+    console.log("距离顶部" + $(".car").offset().top);
+    console.log("高度" + $(document).scrollTop()); // 计算你的屏幕高度
+    if ($(document).scrollTop() > 400) {
+      $("#left").addClass("left-play");
+      $("#right").addClass("right-play");
+    }
+  });
+  $("body").bind("touchmove", function (e) {
+    if ($(document).scrollTop() > 900 || $(document).scrollTop() < 100) {
+      $("#left").removeClass("left-play");
+      $("#right").removeClass("right-play");
+    }
+  });
+  $(".tab2").hide();
+  touch.on($(".mc"), "tap", function (ev) {
+    $(".tab1").show();
+    $(".tab2").hide();
+  });
+  touch.on($(".cz"), "tap", function (ev) {
+    $(".tab2").show();
+    $(".tab1").hide();
+  });
 });
-
-
 
 function setRem() {
   // 设计稿宽度和设备宽度
